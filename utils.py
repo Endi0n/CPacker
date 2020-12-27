5ff21bd98e81a82a431407179651ca40155a3d02
octavian <coctavian10@yahoo.ro>
2020-12-27 21:10:52 +0200

    Added the full path to get_expanded_file_list return output aswell

--- utils.py
+++ utils.py
@@ -9,16 +9,15 @@ def get_expanded_file_list(*files):
 
         # Add file
         if os.path.isfile(file):
-            file_lst.append(file)
+            file_lst.append((file, os.path.realpath(file)))
 
         # Parse folder
         elif os.path.isdir(file):
             for path, subdirs, cfiles in os.walk(file):
                 for name in cfiles:
-                    # Relative file path cropped
-                    file_path = os.path.realpath(os.path.join(path, name))[len(os.getcwd()):]
-
-                    file_lst.append(file_path)
+                    file_path = os.path.realpath(os.path.join(path, name))
+                    relative_file_path = file_path[len(os.getcwd()):]
+                    file_lst.append((relative_file_path, file_path))
         else:
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file)
 
