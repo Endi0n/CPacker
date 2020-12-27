9d9bf45ddc476895125d282273bfc0f8cbd7aeaf
octavian <coctavian10@yahoo.ro>
2020-12-27 20:13:03 +0200

    Added util fct to get all the files for a list of files / dirs

---
+++ utils.py
@@ -0,0 +1,25 @@
+import os
+import errno
+
+
+def get_expanded_files(*files):
+    file_lst = []
+
+    for file in files:
+
+        # Add file
+        if os.path.isfile(file):
+            file_lst.append(file)
+
+        # Parse folder
+        elif os.path.isdir(file):
+            for path, subdirs, cfiles in os.walk(file):
+                for name in cfiles:
+                    file_path = os.path.join(path, name)[len(file):]  # Relative file path cropped
+                    file_lst.append(file_path)
+        else:
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file)
+
+    return file_lst
+
+
