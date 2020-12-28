a6a2a9d93ce580d25847ceaae14ff7048d76bba2
octavian <coctavian10@yahoo.ro>
2020-12-28 13:16:44 +0200

    Solved bug for relative paths

--- utils.py
+++ utils.py
@@ -45,10 +45,13 @@ def get_expanded_file_list(*files):
 
         # Parse folder
         elif os.path.isdir(file):
+            cwd = os.path.normpath(os.path.join(os.path.join(os.getcwd(), file), os.pardir))
+            cwd_prefix = len(cwd) + 1
+
             for path, subdirs, cfiles in os.walk(file):
                 for name in cfiles:
                     file_path = os.path.realpath(os.path.join(path, name))
-                    relative_file_path = file_path[len(os.getcwd()) + 1:]
+                    relative_file_path = file_path[cwd_prefix:]
                     file_lst.append((relative_file_path, file_path))
         else:
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file)
